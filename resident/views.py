a2297274e9e1106108c02a131cac7ddbccc84ee0
Marcos8060 <marcosgav80@gmail.com>
2022-03-18 14:45:00 +0300

    created the user authentication with a login system

--- resident/views.py
+++ resident/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
 
 # Create your views here.
 def home(request):
@@ -6,4 +8,37 @@ def home(request):
 
 # register view
 def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request,'Username already exists')
+            return redirect('register')
+        elif User.objects.filter(email=email).exists():
+            messages.info(request,'Email already exists')
+            return redirect('register')
+        else:
+            user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
+            user.save()
+            return redirect('login')
     return render(request,'register.html')
+
+# login view
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(request,username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'Invalid Credentials')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
