fbfb24addea6f04536ec02a89c150d530ed49e01
Marcos8060 <marcosgav80@gmail.com>
2022-03-18 15:01:51 +0300

    created the hood and business models

--- resident/models.py
+++ resident/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Hood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    occupants_count = models.IntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+class Business(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    hood = models.ForeignKey(Hood,on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
